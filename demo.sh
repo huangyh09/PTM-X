ca2bee5145967527b72ad442c2edd0dc49ac3e36
Yuanhua Huang <huangyh094@gmail.com>
2017-12-18 11:20:30 +0000

    update demo

--- demo.sh
+++ demo.sh
@@ -10,10 +10,17 @@ sample_negative=$dat_dir/interface/crosstalkSamples/negative_samples.txt
 feature_positive=$dat_dir/interface/positive_features.txt
 feature_negative=$dat_dir/interface/negative_features.txt
 
+PTMX-feature -p 20 -i $sample_positive -o $feature_positive -d $dat_dir/data/ #--verbose
+PTMX-feature -p 20 -i $sample_negative -o $feature_negative -d $dat_dir/data/
+
+## alternative option
 # python PTMXtalk/feature_extractor.py -p 20 -i $sample_positive -o $feature_positive -d $dat_dir/data/ #--verbose
 # python PTMXtalk/feature_extractor.py -p 20 -i $sample_negative -o $feature_negative -d $dat_dir/data/
 
 
 ### predict based on features
 result_file=$dat_dir/interface/test_results.txt
-python PTMXtalk/predict.py -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
+PTMX-predict -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
+
+## alternative option
+# python PTMXtalk/predict.py -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
