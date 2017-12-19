6db85b946779d9cb323b00054fc267c7e00536aa
Yuanhua Huang <huangyh094@gmail.com>
2017-12-19 21:43:54 +0000

    update demo.sh

--- demo.sh
+++ demo.sh
@@ -19,24 +19,22 @@ PTMX-feature -p 20 -i $sample_negative -o $feature_negative -d $dat_dir/data/
 
 
 ### predict based on features
-result_file=$dat_dir/interface/test_results.txt
-PTMX-predict -p 20 -N 500 -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
-
+result_file=$dat_dir/interface/test_results_new.txt
+PTMX-predict -s 1 -p -1 -N 100 -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
 
 test_file=$dat_dir/interface/testset/dataset_human_results.txt
 result_file=$dat_dir/interface/test_human_new.txt
 model_file=$dat_dir/interface/trained_model_set.pkl
-PTMX-predict -p 10 -i $test_file -o $result_file -m $model_file
+PTMX-predict -s 1 -p -1 -i $test_file -o $result_file -m $model_file
 
 
 ## alternative option
 # result_file=$dat_dir/interface/test_results_new.txt
-# python PTMXtalk/predict.py -p 20 -N 500 -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
-
+# python PTMXtalk/predict.py -s 1 -p -1 -N 100 -i $feature_positive -o $result_file --positive $feature_positive --negative $feature_negative
 
 # test_file=$dat_dir/interface/testset/dataset_human_results.txt
 # result_file=$dat_dir/interface/test_human_new.txt
 # model_file=$dat_dir/interface/trained_model_set.pkl
-# python PTMXtalk/predict.py -p 10 -i $test_file -o $result_file -m $model_file
+# python PTMXtalk/predict.py -s 1 -p -1 -i $test_file -o $result_file -m $model_file
 
 
